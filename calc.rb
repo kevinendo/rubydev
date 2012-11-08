45a31804a548972e2e6fdb9c7d1a729406eba584
Kevin Endo <kevinendo@gmail.com>
2012-11-07 17:40:50 -0800

    tutorial file

---
+++ calc.rb
@@ -0,0 +1,24 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts 24 * 365
+
+puts 60 * 24 * 365 * 10
+
+puts 'Hello, World!'
+
+puts ''
+
+puts 'Good-bye.'
+
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + ' apple pie.'
+  
